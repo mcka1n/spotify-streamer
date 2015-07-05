324ec88ef94ed75ec46cba94f7e09de319e6d680
Ed Mckain <mckain.default@gmail.com>
2015-07-05 11:31:15 -0600

    adds basic project structure

---
+++ app/src/androidTest/java/com/hacknplay/spotifystreamer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.hacknplay.spotifystreamer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
